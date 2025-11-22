c5948a3573b5c95d2169e51ba9ab5ec94bed1628
melizw <mm1282173@gmail.com>
2025-11-22 00:07:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,70 +1,79 @@
 const express = require("express");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 const path = require("path");
-
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static("public"));
-
 app.use(
     session({
-        secret: "segredo123",
+        secret: "segredo-top",
         resave: false,
-        saveUninitialized: false,
-        cookie: { maxAge: 1000 * 60 * 10 },
+        saveUninitialized: true,
     })
 );
-
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "../public")));
 let produtos = [];
-
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "login.html"));
+    res.redirect("/api/login");
 });
-
+app.get("/login", (req, res) => {
+    res.render("login");
+});
 app.post("/login", (req, res) => {
     const { usuario, senha } = req.body;
 
     if (usuario === "admin" && senha === "123") {
         req.session.user = usuario;
-        res.cookie("ultimoAcesso", new Date().toLocaleString(), {
-            maxAge: 1000 * 60 * 60,
-        });
-        return res.redirect("/produtos");
+        res.cookie("ultimoAcesso", new Date().toLocaleString());
+        return res.redirect("/api/cadastro");
     }
 
-    return res.send("<h3>Usuário ou senha incorretos!</h3><a href='/'>Voltar</a>");
+    res.send("Usuário ou senha inválidos.");
+});
+app.get("/cadastro", (req, res) => {
+    if (!req.session.user) {
+        return res.send("Você precisa fazer login.");
+    }
+
+    const ultimoAcesso = req.cookies.ultimoAcesso || "Primeiro acesso";
+
+    res.render("cadastro", { ultimoAcesso });
+});
+app.post("/cadastro", (req, res) => {
+    if (!req.session.user) {
+        return res.redirect("/api/login");
+    }
+
+    const novoProduto = {
+        codigo: req.body.codigo,
+        descricao: req.body.descricao,
+        precoCusto: req.body.precoCusto,
+        precoVenda: req.body.precoVenda,
+        dataValidade: req.body.dataValidade,
+        estoque: req.body.estoque,
+        fabricante: req.body.fabricante,
+    };
+
+    produtos.push(novoProduto);
+
+    res.redirect("/api/produtos");
 });
 
 app.get("/produtos", (req, res) => {
-    if (!req.session.user) {
-        return res.send("<h3>Você precisa estar logado!</h3><a href='/'>Login</a>");
-    }
-    res.sendFile(path.join(__dirname, "views", "produtos.html"));
-});
+    if (!req.session.user) return res.redirect("/api/login");
 
-app.post("/cadastrar", (req, res) => {
-    if (!req.session.user) {
-        return res.send("Você não está logado!");
-    }
-
-    produtos.push(req.body);
-    res.redirect("/produtos");
-});
-
-app.get("/listar", (req, res) => {
-    res.json({
-        ultimoAcesso: req.cookies.ultimoAcesso || "Nenhum",
-        produtos: produtos,
+    res.render("produtos", {
+        lista: produtos,
+        ultimoAcesso: req.cookies.ultimoAcesso,
     });
 });
 
 app.get("/logout", (req, res) => {
     req.session.destroy();
-    res.redirect("/");
+    res.clearCookie("ultimoAcesso");
+    res.redirect("/api/login");
 });
-
-app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000"));
+module.exports = app;
